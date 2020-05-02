1455450dc1e2e362fa21fba73d864f174c91a1af
Enyo Lin <a11160420@gmail.com>
2020-05-02 14:24:30 +0800

    select.js css adjust

--- src/components/Select.js
+++ src/components/Select.js
@@ -11,59 +11,78 @@ function Select(props) {
     return(
       <div className="selection">
         <div className="select-card">
-          <strong> {props.drinkItem}   NT$ {props.drinkPrice} </strong>
           <button onClick={props.closeSelection}> X </button>
+          <div className="title-grid">
+            <div className="title1"> <strong> {props.drinkItem}  </strong> </div>
+            <div className="title2"> <strong> NT$ {props.drinkPrice} </strong> </div>
+          </div>
           <hr />
           <form onSubmit={props.handleSubmit}>
             <div className="ice">
-              <strong> 冰量： </strong> 
+              <strong> 冰量： </strong>
+              <hr /> 
               { props.ice.map(item => {
                   return(
-                    <label key={item.id}> 
-                      <span> {item.name} </span>
-                      <input type="radio" value={item.name} 
-                              checked={props.iceList === item.name} 
-                              onChange={props.handleIceChange}
-                      />
-                    </label>
+                    <div>
+                      <label key={item.id}> 
+                        <input type="radio" value={item.name} 
+                                checked={props.iceList === item.name} 
+                                onChange={props.handleIceChange}
+                        />
+                        <span> {item.name} </span>
+                      </label>
+                      <br />
+                    </div>
                   );
                 }) 
               }
             </div>
+            <hr />
             <div className="sugar">
               <strong> 甜度： </strong> 
+              <hr />
               { props.sugar.map(item => {
                   return(
-                    <label key={item.id}> 
-                      <span> {item.name} </span>
-                      <input type="radio" value={item.name} 
-                              checked={props.sugarList === item.name} 
-                              onChange={props.handleSugarChange}
-                      />
-                    </label>
+                    <div>
+                      <label key={item.id}> 
+                        <input type="radio" value={item.name} 
+                                checked={props.sugarList === item.name} 
+                                onChange={props.handleSugarChange}
+                        />
+                        <span> {item.name} </span>
+                      </label>
+                      <br />
+                    </div>
                   );
                 }) 
               }
             </div>
+            <hr />
             <div className="ingre">
               <strong> 加料：</strong> 
+              <hr />
               { props.ingredient.map((item, index) => {
                   return(
-                    <label key={item.id}> 
-                      <span> {item.name} </span>
-                      <input type="checkbox" value={item.name} 
-                              checked={props.ingredientList.check} 
-                              onChange={props.handleIngredientChange.bind(this, index)}
-                      />
-                    </label>
+                    <div>
+                      <label key={item.id}> 
+                        <input type="checkbox" value={item.name} 
+                                checked={props.ingredientList.check} 
+                                onChange={props.handleIngredientChange.bind(this, index)}
+                        />
+                        <span> {item.name} </span>
+                      </label>
+                      <br />
+                    </div>
                   );
                 }) 
               }
             </div>
-            <strong> total: {props.singleDrinkTotalPrice} </strong>
+            <hr />
+            <strong> total: NT$ {props.singleDrinkTotalPrice} </strong>
+            <hr />
             <div className="select-button-grid">
                 <button onClick={props.deleteDrinkQuantity}> - </button>
-                <strong> {props.singleDrinkTotal} </strong>
+                <p> <strong> {props.singleDrinkTotal} </strong> </p>
                 <button onClick={() => {props.addDrinkQuantity(); props.addDrinkPrice(props.drinkPrice)}}> + </button>
                 <button disabled={!props.singleDrinkTotal} onClick={()=> {props.closeSelection(); 
                       props.addToCart(props.drinkId, props.drinkItem, props.singleDrinkTotal, props.singleDrinkTotalPrice, 
