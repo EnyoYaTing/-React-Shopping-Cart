3b7e7a8b5bbb4e16738fc939dc77c57ddabbe816
Enyo Lin <a11160420@gmail.com>
2020-05-02 19:13:25 +0800

    create header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
+import Header from './Header';
 import Home from './Home';
 
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
+        <Header />
         <Home />
       </div>  
     );
